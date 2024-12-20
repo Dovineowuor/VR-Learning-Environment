bf46cbcbb0791ba80432c77aed3588ec207667d3
dovineowuor <owuordove@gmail.com>
2024-12-20 12:56:30 +0000

    Update build script to include JSON-C library and improve include paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o VRLearning src/main.c src/core/*.c src/systems/*.c src/ui/*.c -lGL -lGLEW -lglfw -lcurl -lm
+ gcc -I src/core -I src/systems -I src/ui -o VRLearning src/main.c src/core/*.c src/systems/*.c src/ui/*.c -lGL -lGLEW -lglfw -lcurl -lm -ljson-c
