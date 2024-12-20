2b34962cedb3f4ee59032a719348e3b414208528
dovineowuor <owuordove@gmail.com>
2024-12-20 12:58:34 +0000

    Add start_render_loop function prototype to systems.h

--- src/systems/systems.h
+++ src/systems/systems.h
@@ -1,9 +1,9 @@
 #ifndef SYSTEMS_H
 #define SYSTEMS_H
 
-void send_data_to_server(const char *url, const char *data);
 void initialize_tasks();
 void process_tasks();
 void cleanup_tasks();
+void start_render_loop();
 
-#endif
+#endif // SYSTEMS_H
